d06e6e2d1efb27adf17b442c58dc100abd6dfc52
https://github.com/rishabthakur-722 <rishabthakurji722@gmail.com>
2026-05-08 20:50:39 +0530

    Add LOOPS folder with Java files

---
+++ LOOPS/multipleby10.java
@@ -0,0 +1,11 @@
+
+public class multipleby10 {
+
+    public static void main(String[] args) {
+        int n = 100;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i * 10);
+        }
+    }
+}
